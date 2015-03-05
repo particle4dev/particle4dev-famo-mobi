1a2349d73aaaae165201eebcf76594a6283a93a8
particle4dev <particle4dev@gmail.com>
2015-03-05 15:14:03 +0700

    release 0.8.3

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
     summary: "open source front-end framework for developing mobile apps with famous and meteor",
-    version: "0.8.2",
+    version: "0.8.3",
     name: "particle4dev:famo-mobi",
     git: "https://particle4dev@bitbucket.org/particle4devs-team/famo-mobi.git"
 });
